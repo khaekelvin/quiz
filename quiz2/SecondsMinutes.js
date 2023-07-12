26369e188679243fc59ebac984dca4fad342b35e
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2023-07-12 10:58:44 +0100

    Update SecondsMinutes.js

--- quiz2/SecondsMinutes.js
+++ quiz2/SecondsMinutes.js
@@ -1,12 +1,12 @@
-function CalculateTotalSeconds(){//declare function
-    let TotalSeconds = parseInt(prompt("number of seconds"));//no of seconds
+function CalculateTotalSeconds(){  //declare function
+let TotalSeconds = parseInt(prompt("number of seconds"));  //no of seconds
 
-const minute = Math.floor(TotalSeconds / 60);//to find the minutes, Math.floor rounds down the result
+const minute = Math.floor(TotalSeconds / 60);  //to find the minutes, Math.floor rounds down the result
 
-const second = (TotalSeconds % 60);//to find the seconds remaining
+const second = (TotalSeconds % 60);  //to find the seconds remaining
 
-console.log(minute);//print minute
-console.log(second);//print second
+console.log(minute);  //print minute
+console.log(second);  //print second
 }
 
 CalculateTotalSeconds();//call function
