1546b7ab94365255915350b035456bec8f5e055c
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2023-07-12 10:58:00 +0100

    Update StudentYear.js

--- quiz2/StudentYear.js
+++ quiz2/StudentYear.js
@@ -1,27 +1,27 @@
-function CreditYear(){//declare function
-    let credits = prompt(parseInt(("How many credits?")));//ask for credits
+function CreditYear(){  //declare function
+let credits = prompt(parseInt(("How many credits?")));  //ask for credits
 let year;//declare year
 
 if (credits <= 23){
-    year= "freshman";//23 or less
+    year= "freshman";  //23 or less
 }
 
 else if (credits > 24 && credits < 53 ){
-    year="sophomore" ;//between 24 and 53
+    year="sophomore";  //between 24 and 53
 }
 
 else if (credits > 53 && credits < 83){
-    year ="junior ";//between 53 and 83
+    year ="junior";  //between 53 and 83
 }
 
 else {
-    year = "senior";// credits > 84 and over
+    year = "senior";  // credits > 84 and over
 }
 
-console.log("you are a " + year)//print year
+console.log("you are a " + year)  //print year
 }
 
-CreditYear();//call function
+CreditYear();  //call function
 
 
 
