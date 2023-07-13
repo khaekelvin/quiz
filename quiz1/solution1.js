8866df142ce30f77e55965777f5e29f30ee89aa4
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2023-07-13 15:46:27 +0100

    Update solution1.js
    corrected issues

--- quiz1/solution1.js
+++ quiz1/solution1.js
@@ -1,6 +1,6 @@
-let item1 = x;
-x = prompt("Enter the cost of item1")
-let item2 = y;
-y = prompt("Enter the cost of item2")
-let TotalCost = (x + y);
+let  item1, item2;
+item1 = parseInt(prompt("Enter the cost of item1"));
+item2 = parseInt(prompt("Enter the cost of item2"));
+let TotalCost = (item1 + item2);
 console.log("The total cost is" + TotalCost);
+alert(typeof TotalCost)
