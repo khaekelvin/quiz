f59e32cc8b866130e95d6f7a8a68358fa45b98bd
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2023-07-14 16:39:10 +0100

    Update 50RandomVariables.js

--- quiz2/50RandomVariables.js
+++ quiz2/50RandomVariables.js
@@ -1,22 +1,6 @@
 for
     ( let i = 0; i < 50; i++){
-    let random = Math.floor(Math.random() *4) + 3; //math.random returns a random number from 0
+    let random = Math.floor(Math.random() *4) + 3;  //math.random returns a random number from 0
 
-    console.log(random);  //print random numbers between 3 and 6
-    }
-
-
-    
-   /* 
-    function RandomIntegers(){//function
-        for
-        ( let i = 0; i < 50; i++){
-            let random = Math.floor(Math.random() *4) + 3;//math.random returns a random number from 0
-            return random;
-    
-            
-        }
-    
-    }
-    
-    RandomIntegers();//print random numbers between 3 and 6 */
+    console.log(random)  //print random numbers between 3 and 6
+}
