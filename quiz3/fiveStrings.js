dcd4da7e01404337ae4e2697535d71ff927b7efb
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2023-07-17 10:05:43 +0100

    Add files via upload

---
+++ quiz3/fiveStrings.js
@@ -0,0 +1,13 @@
+function conNum(){
+let a = prompt("Enter 1st number:");
+let b = prompt("Enter 2nd number:");
+let c = prompt("Enter 3rd number:");
+let d = prompt("Enter 4th number:");
+let e = prompt("Enter 5th number:");
+let o = '+';
+let p = "'";
+
+let allnum = console.log(p+a+o+b+o+c+o+d+o+e+p);
+}
+
+conNum();
---
+++ quiz3/kiloToPounds.js
@@ -0,0 +1,10 @@
+function wToP(){
+    let weight = parseInt(prompt("Enter your weight in kg:"));
+
+while (weight < 0 && weight != number){
+    console.log("invalid entry");
+}
+
+let convert = console.log("your weight in pounds is:" +weight * 2.20462 +"pounds");
+}
+wToP();
